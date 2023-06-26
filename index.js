d5da7aa4b632b6b66ccc92a6fcb82b89d9007539
Allan Saenz <allan.d.saenz@gmail.com>
2023-06-26 17:32:29 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+
+  document.addEventListener("DOMContentLoaded", function(){
+    const cool = document.getElementById("text");
+    cool.textContent = "This is really cool!"
+  })
